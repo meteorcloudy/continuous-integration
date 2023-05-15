e6625a3dd1896128dde5077a0e1b2640fc7bbea4
Yun Peng <pcloudy@google.com>
2023-05-15 17:55:46 +0200

    Get latest builds even still running

--- buildkite_statics.py
+++ buildkite_statics.py
@@ -1,18 +1,16 @@
 import json
+import os
 import requests
 import datetime
 
 """ Run python3 buildkite_statics.py > output and copy the content to a spreadsheet to analyze """
 
 # Replace `YOUR_API_TOKEN` with your Buildkite API access token.
-api_token = "YOUR_API_TOKEN"
+api_token = os.environ["BUILDKITE_API_TOKEN"]
 
 # Replace `YOUR_ORGANIZATION` and `YOUR_PIPELINE` with the name of the organization and pipeline you want to query.
 organization = "bazel"
 pipeline = "bazel-bazel"
-
-# Construct the URL for the API request.
-url = f"https://api.buildkite.com/v2/organizations/{organization}/pipelines/{pipeline}/builds?state=passed"
 
 # Set the API authentication headers.
 headers = {
@@ -20,12 +18,12 @@ headers = {
     "Content-Type": "application/json"
 }
 
-MAX_PAGES = 20
 
 def download_builds():
+    MAX_DOWNLOAD_PAGES = 1
     page_number = 1
-    while page_number <= MAX_PAGES:
-        url = f"https://api.buildkite.com/v2/organizations/{organization}/pipelines/{pipeline}/builds?state=passed&page={page_number}&per_page=100"
+    while page_number <= MAX_DOWNLOAD_PAGES:
+        url = f"https://api.buildkite.com/v2/organizations/{organization}/pipelines/{pipeline}/builds?page={page_number}&per_page=100"
 
         # Make the API request and parse the JSON response.
         response = requests.get(url, headers=headers)
@@ -49,8 +47,9 @@ def get_run_time_for_job(job):
     return (datetime.datetime.fromisoformat(job["finished_at"].replace('Z', '+00:00')) - datetime.datetime.fromisoformat(job["started_at"].replace('Z', '+00:00'))).total_seconds()
 
 def analyze():
+    MAX_ANALYZE_PAGES = 1
     page_number = 1
-    while page_number <= MAX_PAGES:
+    while page_number <= MAX_ANALYZE_PAGES:
         # Load json file from a file
         f = open(f"data/builds_{page_number}.json", "r")
         builds_for_page = json.load(f)
@@ -58,10 +57,10 @@ def analyze():
 
         # Iterate over the builds on the current page.
         for build in builds_for_page:
-            if build["state"] == "passed" and build["branch"] == "master":
+            if build["branch"] == "master":
                 for job in build["jobs"]:
                     name = job.get("name", None)
-                    if name == ":darwin: (OpenJDK 11, Xcode)":
+                    if name == ":darwin: (OpenJDK 11, Xcode)" and job["state"] == "passed":
                         print(job["created_at"] + "\t" + job["web_url"] + "\t" + str(get_wait_time_for_job(job)//60) + "\t" + str(int(get_run_time_for_job(job)//60)))
                         # print("\n\n+++ For job " + job["web_url"] + ":")
                         # print(name + " waited " + str(get_wait_time_for_job(job)) + " seconds" + ", ran for " + str(int(get_run_time_for_job(job)) // 60) + " minutes")
@@ -69,7 +68,7 @@ def analyze():
         page_number += 1
 
 def main():
-    # download_builds()
+    download_builds()
     analyze()
 
 if __name__ == "__main__":
